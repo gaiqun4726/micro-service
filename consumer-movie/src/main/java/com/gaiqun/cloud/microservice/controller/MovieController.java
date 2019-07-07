3a8d25b5a37322a113ba1acba27db973fe2abc3f
gaiqun <gaiqun@itiger.com>
2019-07-07 09:06:57 +0800

    feat: read url from config.

--- consumer-movie/src/main/java/com/gaiqun/cloud/microservice/controller/MovieController.java
+++ consumer-movie/src/main/java/com/gaiqun/cloud/microservice/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.gaiqun.cloud.microservice.controller;
 
 import com.gaiqun.cloud.microservice.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,11 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${user.userServiceUrl}")
+    private String userServiceUrl;
+
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
-        return restTemplate.getForObject(String.format("http://localhost:8000/user/%d", id), User.class);
+        return restTemplate.getForObject(String.format(userServiceUrl + "/%d", id), User.class);
     }
 }
