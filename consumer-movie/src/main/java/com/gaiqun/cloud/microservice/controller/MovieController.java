718321685b7949cfb598ee3cc0a50512d616a4b2
gaiqun <gaiqun@itiger.com>
2019-07-08 12:13:09 +0800

    feat: get endpoint of eureka.

--- consumer-movie/src/main/java/com/gaiqun/cloud/microservice/controller/MovieController.java
+++ consumer-movie/src/main/java/com/gaiqun/cloud/microservice/controller/MovieController.java
@@ -1,8 +1,11 @@
 package com.gaiqun.cloud.microservice.controller;
 
 import com.gaiqun.cloud.microservice.pojo.User;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +20,19 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Value("${user.userServiceUrl}")
     private String userServiceUrl;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
         return restTemplate.getForObject(String.format(userServiceUrl + "/%d", id), User.class);
+    }
+
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showInfo() {
+        return discoveryClient.getInstances("microservice-provider-user");
     }
 }
